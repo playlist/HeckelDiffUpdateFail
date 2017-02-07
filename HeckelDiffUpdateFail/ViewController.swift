4b9f440d15be6d5261882158928e47317c7f8d09
Alex Tang <altitude@funkware.com>
2017-02-07 11:40:18 -0800

    Initial Commit

---
+++ HeckelDiffUpdateFail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeckelDiffUpdateFail
+//
+//  Created by Alex Tang on 2/7/17.
+//  Copyright © 2017 Funkware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
